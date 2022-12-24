d28a13d8ae37b5262e9235ad63d4e340f11dbfd7
Ayush <61765149+citricacid2@users.noreply.github.com>
2022-12-24 21:10:06 +0530

    construction

---
+++ components/PrivacyScreen.tsx
@@ -0,0 +1,10 @@
+export default function PrivacyScreen() {
+  return (
+    <div className="bg-black fixed top-0 left-0 w-full h-screen z-50 flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold text-center">
+        This page is currently under construction.
+      </h1>
+      <p className="text-center">Please check back later.</p>
+    </div>
+  );
+}
