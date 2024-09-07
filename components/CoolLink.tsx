b04c3fc74786a53d4d4f77f47b80f1aa4227914e
Taylor1117 <144968957+Taylor1117@users.noreply.github.com>
2024-09-07 18:38:07 -0400

    Color Change

--- components/CoolLink.tsx
+++ components/CoolLink.tsx
@@ -16,7 +16,7 @@ export default function CoolLink({ text, href }: CoolLinkProps) {
     <Link
       href={href}
       className="text-blue-500 font-bold hover:py-1 hover:px-3 hover:bg-blue-500 hover:text-white rounded-lg transition-all break-words"
-      /*className="text-blue-500 font-bold hover:py-1 hover:px-3 hover:bg-blue-500 hover:text-white rounded-lg transition-all whitespace-nowrap"*/
+      /*className="text-[#949CE0] font-bold hover:py-1 hover:px-3 hover:bg-[#949CE0] hover:text-white rounded-lg transition-all whitespace-nowrap"*/
     >
       {text} <FontAwesomeIcon icon={faExternalLink} />
     </Link>
